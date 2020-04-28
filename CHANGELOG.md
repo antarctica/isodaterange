4fa2de2f3d9892c87752f64c33d031dbe181fcad
Tim Barnes <tdba@bas.ac.uk>
2020-04-28 14:32:52 +0100

    added support for durations (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+## 1.0.0 [2020-04-28]
+
+### Added
+
+* (#1) - Added support for durations
+
 ## 0.1.1 [2020-01-16]
 
 ### Fixed
@@ -9,7 +15,7 @@
 ### Modified
 
 * (#3) - Dates with no dates are now handled more efficiently
- 
+
 ### Removed
 
 * (#2) - Removed references to API project
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name             = "isodaterange",
-    version          = "0.1.1",
+    version          = "1.0.0",
     author           = "Tim Barnes",
     author_email     = "tdba@bas.ac.uk",
     url              = "https://gitlab.data.bas.ac.uk/uk-pdc/utils/isodaterange",
